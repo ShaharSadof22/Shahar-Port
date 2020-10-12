b11de98c933f5501238a4dd05dfb265655023ec9
Shahar Sadof <shaharsadof22@gmail.com>
2020-10-12 23:02:53 +0300

    almost the end

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,30 +3,46 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
     }
 
     return (
-      <footer>
+      <footer >
+        <div className="contact-details-footer">
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
 
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
+              <h4>Contact Details</h4>
+              <p className="address">
+                Shahar Sadof<br />
+                  igael Yadin 38 <br />
+                  Herzlia<br />
+                  0546595954<br />
+                <span>shaharsadof22@gmail.com</span>
+              </p>
+            </div>
+          </aside>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+
+        <div className="icons-details-footer">
+          <div className="twelve columns">
+            <ul className="social-links">
+              {networks}
+            </ul>
+
+            <ul className="copyright">
+              <li>&copy; Shahar Sadof Web Developer</li>
+              <li>Design by <a title="Styleshout" href="#home">Shahar Sadof</a></li>
+            </ul>
+
+          </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+        </div>
+      </footer>
     );
   }
 }
